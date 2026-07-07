504917e8b6fee1081e06789563455f97308cf544
Shakti Singh <shakti33123singh@gmail.com>
2026-07-07 18:07:28 +0530

    ecma standards basic and datatypes

---
+++ javascript/01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) // in browser not in editor
+
+console.log(3
+    +
+    3) // dont write just because you can, readilibility shoud be high
+
+
+console.log("hitesh")    
+
+let name = "Shakti"
+let number = 12
+let loggedIn = true
+let state;
+
+//Primitive data types
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+
+//Non-Primtitiive
+//Object
+
+console.log(typeof(null)); // output => object
+console.log(typeof(undefined)); //output => undefined
