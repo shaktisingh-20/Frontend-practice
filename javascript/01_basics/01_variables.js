c8ef316b3e9459b2d330f0d3a570b6e5bbe0e0d5
Shakti Singh <shakti33123singh@gmail.com>
2026-07-07 17:34:05 +0530

    learned variables let var const and learned why not to use var

---
+++ javascript/01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accoundEmail  = "shakti@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountName;
+
+// accountId = 2457534 not allowed
+
+accoundEmail= "baau@gamil.com"
+accountPassword= 122423
+accountCity = "Bareilly"
+/*
+Prefer not to use var because of its issue with scope(block, functional, loops scope) 
+*/
+
+console.log(accoundEmail);
+console.table([accoundEmail, accountPassword, accountCity, accountName])
+
