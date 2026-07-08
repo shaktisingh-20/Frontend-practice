42a91ecc2051af42fb7a6614d1838406e3a2580c
Shakti Singh <shakti33123singh@gmail.com>
2026-07-08 13:16:39 +0530

    compariosn in js how comparison works

---
+++ javascript/01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+console.log(null == 0);
+console.log(null >0);
+console.log(null >= 0);
+
+// Comparison convert null to a number, treating it as 0;
+// thats why (null >= 0) is true &
+//(null>0) is flase
+
+console.log("2">1);
+console.log(2<"1");
+
+
+// undef
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+
+// "===" strit equality check
+
+console.log("2" == 2);
+console.log("2" === 2);
+
