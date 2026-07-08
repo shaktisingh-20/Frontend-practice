050cc018368e56acaf9574d07ee43ccae8cfd432
Shakti Singh <shakti33123singh@gmail.com>
2026-07-08 12:55:28 +0530

    conversion operation in js

--- javascript/01_basics/03_conversionOperation.js
+++ javascript/01_basics/03_conversionOperation.js
@@ -3,24 +3,24 @@ let score = "3eff3a"
 let nonumber = null
 let undefcheck = undefined
 
-console.log(typeof(score))
+// console.log(typeof(score))
 
 let numberInValue = Number (score)
-console.log(typeof(numberInValue))
-console.log(numberInValue);
+// console.log(typeof(numberInValue))
+// console.log(numberInValue);
 
 let numberInhat = Number (hat)
-console.log(typeof(numberInhat))
-console.log(numberInhat);
+// console.log(typeof(numberInhat))
+// console.log(numberInhat);
 
 let nullchecker = Number (nonumber)
-console.log(typeof(nullchecker))
-console.log(nullchecker);
+// console.log(typeof(nullchecker))
+// console.log(nullchecker);
 
 
 let undef = Number (undefcheck)
-console.log(typeof(undef))
-console.log(undef);
+// console.log(typeof(undef))
+// console.log(undef);
 
 // 33 = Number => 33
 // "33adc" = Number => Nan
@@ -30,14 +30,45 @@ console.log(undef);
 let booleanLogged = "Shakti"
 
 let booleanIsLogged = Boolean(booleanLogged)
-console.log(booleanIsLogged)
+// console.log(booleanIsLogged)
 
 // "" = Boolean => false
 // "Shakti"= Boolean => true
 
-let str = 33
+let str = "0"
 let strToInt = String (str)
-console.log(typeof(strToInt));
-console.log(strToInt);
+// console.log(typeof(strToInt));
+// console.log(strToInt);
+
+
+/* ***************Operations****************8 */
+
+let value= 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "Hello"
+let str2 = " Shakti"
+let str3= str1 + str2
+console.log(str3);
+
+console.log("1" + 2 );
+console.log(1 + "2");
+console.log("1" + 2 +2);
+console.log(1 +2 +"2");
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
 
 
