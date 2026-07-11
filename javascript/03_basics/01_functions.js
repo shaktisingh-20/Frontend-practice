baa337be23bf3961f86df6091691df5eebe5d898
Shakti Singh <shakti33123singh@gmail.com>
2026-07-11 18:30:27 +0530

    understood functions

---
+++ javascript/03_basics/01_functions.js
@@ -0,0 +1,69 @@
+function addTwoNumbers(num1, num2) {
+    // console.log(num1+num2)
+}
+addTwoNumbers(2,3)
+
+// const sum = addTwoNumbers(2,5)
+// console.log(sum); // undefined because we havent added return statement yet 
+
+function addNumbers(num1, num2) {
+    // let num3 = (num1+num2)
+    return num1 + num2 // another way 
+    console.log("shakti"); // this will never print as after return the code in unreachable
+}
+const sum = addNumbers(2,5)
+// console.log("result:=> ",sum);
+
+
+function userLoginMessage(message){
+    if(!message ){
+        console.log("Please enter a name")
+        return
+    }
+    return `${message} just logged in`
+}
+console.log(userLoginMessage())
+
+function userMessage(message = "sam"){
+    if(!message ){
+        console.log("Please enter a name")
+        return
+    }
+    return `${message} said hello hyow are you`
+}
+console.log(userMessage())
+
+function calculateCartPrice(...num){// rest and spread syntax is same but depends on the use case
+    return num
+}
+console.log(calculateCartPrice(200,451,623,65,343));
+
+function addCartPrice(val1, val2, ...num){
+    return num
+}
+console.log(addCartPrice(234,456,24,521,234))
+
+//
+
+const user = {
+    name:"Shakti",
+    age:21
+}
+
+function handleObject(anyObject){
+    return `This is ${anyObject.name} and he is ${anyObject.age} years old`
+}
+
+// console.log(handleObject(user))
+console.log(handleObject({
+    name:"Shakti",
+    age:22
+}))
+
+const myNewArray = [100,200,300,400,500]
+
+function returnSecondElement(arr){
+    return arr[1]
+}
+console.log(returnSecondElement(myNewArray))
+console.log(returnSecondElement([200,300,400,500,600]))
