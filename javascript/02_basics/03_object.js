1531db710e912894d0b5d60f2b47dadf89d3a280
Shakti Singh <shakti33123singh@gmail.com>
2026-07-11 12:11:25 +0530

    learning objects

---
+++ javascript/02_basics/03_object.js
@@ -0,0 +1,12 @@
+const jsUser ={
+    name: "Shakti",
+    "full Name":"Shakti Singh",
+    age:18,
+    location:"Bareilly",
+    email:"shakti@gmail.com",
+    isLoggenIn:false,
+    lastLoginDays:["Monday", "Tuesday"]
+}
+console.log(jsUser.email);
+
+console.log(jsUser["full Name"]);
