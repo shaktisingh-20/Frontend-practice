1f5f34159f420f5b836bce52910ad8993ff33233
Shakti Singh <shakti33123singh@gmail.com>
2026-07-17 10:08:48 +0530

    started contrl flow

---
+++ javascript/04_control_flow/one.js
@@ -0,0 +1,21 @@
+const loggedIn =  true
+const temperature = 41
+
+if(temperature === 50){
+    console.log(`less than 50`);
+}
+else{
+    console.log(`more that 50`);
+}
+// console.log(temperature);
+
+// // <,>,<=,>=, !=, ==, ===\
+
+const score  = 200
+if(score >100){
+    let power = "fly"
+    console.log(`The power is : ${power}`);
+}
+
+const balance = 1000
+// if(balance < 500) console.log("Debit some cash"); // not readable or never used
